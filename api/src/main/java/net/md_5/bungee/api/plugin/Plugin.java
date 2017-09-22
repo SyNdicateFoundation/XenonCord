1741515f237f815b41f42a37eaf5c5bf46bcd665
Minecrell <minecrell@minecrell.net>
2017-09-22 13:15:09 +0200

    Allow plugins to use SLF4J for logging

--- api/src/main/java/net/md_5/bungee/api/plugin/Plugin.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/Plugin.java
@@ -44,6 +44,13 @@ public class Plugin
         // init( proxy, description );
     }
 
+    // Waterfall start - Allow plugins to use SLF4J for logging
+    public org.slf4j.Logger getSLF4JLogger() {
+        return org.slf4j.LoggerFactory.getLogger(logger.getName());
+    }
+    // Waterfall end
+
+
     /**
      * Called when the plugin has just been loaded. Most of the proxy will not
      * be initialized, so only use it for registering
