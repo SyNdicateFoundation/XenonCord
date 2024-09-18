186ef3a34dc9d04ba9b377c1bcc61729f48fa369
realstresser <rezaebr3234s4@gmail.com>
2024-09-18 21:04:41 +0330

    bug reporting

--- proxy/src/main/java/ir/xenoncommunity/modules/listeners/MotdChanger.java
+++ proxy/src/main/java/ir/xenoncommunity/modules/listeners/MotdChanger.java
@@ -2,7 +2,6 @@ package ir.xenoncommunity.modules.listeners;
 
 import ir.xenoncommunity.XenonCore;
 import ir.xenoncommunity.annotations.ModuleListener;
-import ir.xenoncommunity.modules.ModuleManager;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -14,7 +13,7 @@ import net.md_5.bungee.event.EventHandler;
 @ModuleListener public class MotdChanger implements Listener {
     @EventHandler public void proxyPingEvent(final ProxyPingEvent e){
         final ServerPing serverPing = e.getResponse();
-        serverPing.setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&', XenonCore.instance.getConfigData().getModules().getMotd())));
+        serverPing.setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&', XenonCore.instance.getConfigData().getModules().getMotd().replace("ONLINE", String.valueOf(XenonCore.instance.getBungeeInstance().getOnlineCount())))));
         e.setResponse(serverPing);
     }
 }
